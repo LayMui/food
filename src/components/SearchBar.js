ac7c3941036a6502c7651d3dd23aced6fa52f068
LayMui <laymui.toh@taiger.com>
2021-03-14 13:39:10 +0800

    Initial Commit

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,21 +1,35 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, TextInput, StyleSheet } from 'react-native';
+import { Feather } from '@expo/vector-icons';
 
 const SearchBar = () => {
   return (
-    <View style={styles.background}>
-      <Text>Search Bar</Text>
+    <View style={styles.backgroundStyle}>
+    <Feather name="search" style={styles.iconStyle}/>
+      <TextInput style={styles.inputStyle} placeholder="Search"/>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-    background: {
+    backgroundStyle: {
+        marginTop: 10,
         backgroundColor: '#F0EEEE',
         height: 50,
         borderRadius: 5, 
-        marginHorizontal: 15
+        marginHorizontal: 15,
+        flexDirection: 'row'
+    }, 
+    inputStyle: {
+       fontSize: 18,
+        flex: 1
+    },
+    iconStyle: {
+        fontSize: 35,
+        alignSelf: 'center',
+        marginHorizontal: 2
+
     }
 });
 
-export default Searchbar;
+export default SearchBar;
